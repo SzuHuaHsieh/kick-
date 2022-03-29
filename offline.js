b555cabc9b7126132d45eea603d8618963949744
SzuHuaHsieh <102577512+SzuHuaHsieh@users.noreply.github.com>
2022-03-29 15:39:14 +0800

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1648539474,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/wall_up-sheet0.png",
+		"images/wall-sheet0.png",
+		"images/bar-sheet0.png",
+		"images/brick-sheet0.png",
+		"images/ball-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"media/mixkit-arcade-game-explosion-1699.m4a",
+		"media/boom.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
